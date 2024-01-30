2daad500214f40e22265a8a555e62576e9b531f3
MarcosCunhaDev <marcoswrc2@gmail.com>
2024-01-30 12:21:13 -0300

    Using Heading component

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { VStack, Image, Center, Text } from "native-base";
+import { VStack, Image, Center, Text, Heading } from "native-base";
 import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
 
@@ -16,6 +16,11 @@ export const SignIn = () => {
         <Text color="gray.100" fontSize="sm">
           Treine sua mente e seu corpo
         </Text>
+      </Center>
+      <Center>
+        <Heading color="gray.100" fontFamily="heading" fontSize={"xl"} mb={"6"}>
+          Acesse sua conta
+        </Heading>
       </Center>
     </VStack>
   );
